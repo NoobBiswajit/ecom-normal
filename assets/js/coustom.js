73e12b07f6f26ff49958acea9fd75308bd28937a
noob-biswajit <developerbiswajit51@gmail.com>
2023-09-14 13:15:03 +0600

    first commit

---
+++ assets/js/coustom.js
@@ -0,0 +1,85 @@
+var swiper = new Swiper(".heroslider", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+
+// ===========Product-slider=========
+
+var swiper = new Swiper(".sellProduct", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1280: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
+
+// ============New product load more==============
+$(function () {
+    $(".new-product").slice(0, 4).show();
+    $("body").on('click touchstart', '.load-more', function (e) {
+        e.preventDefault();
+        $(".new-product:hidden").slice(0, 4).slideDown();
+        if ($(".new-product:hidden").length == 0) {
+            $(".load-more").css('visibility', 'hidden');
+        }
+        // $('html,body').animate({
+        //     scrollTop: $(this).offset().top
+        // }, 500);
+    });
+});
+// ============New product load more==============
+$(function () {
+    $(".toprated").slice(0, 4).show();
+    $("body").on('click touchstart', '.topretedProload', function (e) {
+        e.preventDefault();
+        $(".toprated:hidden").slice(0, 4).slideDown();
+        if ($(".toprated:hidden").length == 0) {
+            $(".topretedProload").css('visibility', 'hidden');
+        }
+        // $('html,body').animate({
+        //     scrollTop: $(this).offset().top
+        // }, 500);
+    });
+});
+
+
+jQuery(function ($) {
+    $('.menu-btn').click(function () {
+        $('.dropdown').toggleClass('expand')
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+
+    fontFamily: {
+      poppins: ['Poppins']
+    },
+    extend: {
+      colors: {
+        cultured: '#F6F6F8',
+        serpent: '#56CFE1',
+        taupeGray: '#878787',
+        vred: '#CC1512',
+        rbalck: '#222222',
+      }
+    },
+  },
+  plugins: [],
+}
